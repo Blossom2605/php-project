f79cf87d67d1a403f02a0722364015a6a3399195
Blossom2605 <yule2605@naver.com>
2024-05-28 19:26:31 +0900

    폰트 부분 수정

--- charSearch.php
+++ charSearch.php
@@ -20,9 +20,6 @@
         <link rel="stylesheet" type="text/css" href="./css/common.css">
         <link rel="stylesheet" type="text/css" href="./css/search.css">
         <link rel="stylesheet" type="text/css" href="./css/header.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>    
-        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet">
     </head>
     <body>
     <header>
